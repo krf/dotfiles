04956437c2b5b2bf42b53282ebd7f9f9018e1756
Kevin Funk <krf@electrostorm.net>
2010-10-27 08:33:05 +0200

    Fix update-repositories script wrt files/dir

--- bin/update-repositories.sh
+++ bin/update-repositories.sh
@@ -5,9 +5,15 @@ local CWD=$PWD
 echo "*** Fetching new objects from repositories ***"
 
 for i in ./*; do
+    # continue if $i is no directory
+    test -d $i || continue
+
     echo "*** Checking ${i}... ***"
-    test -x $i/.svn && svn up $i
-    test -x $i/.git && (cd $i; git fetch; cd $PWD)
+    # update repository depending on VCS used
+    test -x $i/.svn &&
+        svn up $i
+    test -x $i/.git &&
+        (cd $i; git fetch; cd $PWD)
 done
 
 cd $CWD
