b1e511ea142f3510fa443d59c1c2a8dc96cbfb70
Kevin Funk <krf@electrostorm.net>
2011-01-11 09:13:06 +0100

    Update update-repositories and gitconfig

--- bin/update-repositories.sh
+++ bin/update-repositories.sh
@@ -6,14 +6,14 @@ echo "*** Fetching new objects from repositories ***"
 
 for i in ./*; do
     # continue if $i is no directory
-    test -d $i || continue
+    test -d "$i" || continue
 
     echo "*** Checking ${i}... ***"
     # update repository depending on VCS used
-    test -x $i/.svn &&
-        svn up $i
-    test -x $i/.git &&
-        (cd $i; git fetch; cd $PWD)
+    test -x "$i/.svn" &&
+        svn up "$i"
+    test -x "$i/.git" &&
+        (cd "$i"; git fetch; cd "$PWD")
 done
 
 cd $CWD
