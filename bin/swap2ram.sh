addbcb21c4e91b360f950ecd09967a15dd2e72c2
Kevin Funk <krf@electrostorm.net>
2011-06-21 16:12:28 +0200

    Add swap2ram.sh

---
+++ bin/swap2ram.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+err="not enough RAM to write swap back, nothing done"
+mem=`free|grep Mem:|awk '{print $4}'`
+swap=`free|grep Swap:|awk '{print $3}'`
+test $mem -lt $swap && echo -e $err && exit 1
+swapoff -a && swapon -a &&
+exit 0
