11aee2082550c93275d33a2123fb4a54a977fe3a
Kevin Funk <krf@electrostorm.net>
2011-06-21 11:10:11 +0200

    Improve .kde/env/profile.sh

--- .kde/env/profile.sh
+++ .kde/env/profile.sh
@@ -1,5 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
+# debug output
+#set -x
+
+# prepend $HOME/bin to $PATH
 if [ -d $HOME/bin ]; then
     PATH=${PATH}:$HOME/bin
     export PATH
@@ -10,3 +14,9 @@ export GRE_HOME=/tmp
 
 # fix kmix
 export KMIX_PULSEAUDIO_DISABLE=1
+
+# fix nvidia card painting issues
+VGA_INFO="$(lspci | grep "VGA")"
+if [[ "$VGA_INFO" =~ "nVidia" ]]; then
+    export QT_GRAPHICSSYSTEM=raster
+fi
