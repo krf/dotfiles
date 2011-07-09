123ff34f148dc24e41301da4882668d35e9d796e
Kevin Funk <krf@electrostorm.net>
2011-07-09 14:54:38 +0200

    update-repositories.sh: Add script support

--- bin/update-repositories.sh
+++ bin/update-repositories.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
+#
+# This will update all repositories in $PWD/* (no recursive)
+# Supported types: CVS, SVN, Git, HG
+#
+# Additional script support for: $PWD/*/.update-procedure.sh
+#   This script (if existant) will called directly
+#
+#   Example script (placed in Qt5 repository)
+#     $ cat qt5/.update-procedure.sh
+#
+#     #!/bin/sh
+#
+#     THIS_DIR="$(cd $(dirname $0) && pwd)"
+#     ${THIS_DIR}/qtrepotools/bin/qt5_tool -p
+#
 
 local CWD=$PWD
 
@@ -9,6 +24,13 @@ for i in ./*; do
     test -d "$i" || continue
 
     echo "*** Checking ${i}... ***"
+
+    # call update script if there is any
+    test -x "$i/.update-procedure.sh" && (
+        cd "$i"
+        ./.update-procedure.sh
+        cd "$PWD"
+    )
 
     # update repository depending on VCS used
     test -x "$i/.svn" &&
