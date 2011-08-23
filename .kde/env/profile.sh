ef98445ef02f34f9bb9beed368d9293608257888
Kevin Funk <krf@electrostorm.net>
2011-08-23 07:41:05 +0200

    profile.sh: Remove pulseaudio work-around

--- .kde/env/profile.sh
+++ .kde/env/profile.sh
@@ -12,9 +12,6 @@ fi
 # fix eclipse
 export GRE_HOME=/tmp
 
-# fix kmix
-export KMIX_PULSEAUDIO_DISABLE=1
-
 # fix nvidia card painting issues
 VGA_INFO="$(lspci | grep "VGA")"
 #if [[ "$VGA_INFO" =~ "nVidia" ]]; then
