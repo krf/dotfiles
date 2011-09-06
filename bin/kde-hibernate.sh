b1382ccafdd4ba520fccacd6ebcd77bbc13e2eda
Kevin Funk <krf@electrostorm.net>
2011-09-06 08:17:14 +0200

    Adjust kde shutdown scripts

--- bin/kde-hibernate.sh
+++ bin/kde-hibernate.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-qdbus org.kde.kded /modules/powerdevil suspend 4
+qdbus org.kde.kded /org/freedesktop/PowerManagement Hibernate
--- bin/kde-suspend.sh
+++ bin/kde-suspend.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-qdbus org.kde.kded /modules/powerdevil suspend 1
+qdbus org.kde.kded /org/freedesktop/PowerManagement Suspend
