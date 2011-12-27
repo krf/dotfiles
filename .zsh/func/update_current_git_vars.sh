4eb807331f3a22e0396ea0c233d1ff59bc996490
Kevin Funk <krf@electrostorm.net>
2011-12-27 12:47:45 +0100

    Make ZSH-Git prompt a lot faster

--- .zsh/func/update_current_git_vars.sh
+++ .zsh/func/update_current_git_vars.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 unset __CURRENT_GIT_BRANCH
 unset __CURRENT_GIT_BRANCH_STATUS
 unset __CURRENT_GIT_BRANCH_STATUS_AMOUNT
@@ -6,43 +8,56 @@ unset __CURRENT_GIT_BRANCH_IS_DIRTY
 unset __CURRENT_GIT_BRANCH_REMOTE
 unset __CURRENT_GIT_BRANCH_MERGE
 
-# optimization: do not run in ~
-GIT_ROOT=$(git rev-parse --show-toplevel 2>/dev/null)
-if [[ "$GIT_ROOT" = "$HOME" ]]; then
+__CURRENT_GIT_ROOT="$(git rev-parse --show-toplevel 2>/dev/null)"
+if [[ "$?" -ne "0" ]]; then
     return
 fi
 
-local st="$(git status -uno 2>/dev/null)"
-if [[ -n "$st" ]]; then
-	local -a arr
-	arr=(${(f)st})
+# fill generic git variables
+local branch="$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')"
+local ref="$(git symbolic-ref HEAD)"
+local head="${ref#refs/heads/}"
+local remote="$(git config branch.$head.remote)"
+local merge="$(git config branch.$head.merge)"
+local upstream="$remote/${merge#refs/heads/}"
 
-	if [[ $arr[1] =~ 'Not currently on any branch.' ]]; then
-		__CURRENT_GIT_BRANCH='no-branch'
-	else
-		__CURRENT_GIT_BRANCH="${arr[1][(w)4]}";
-	fi
+local upstream_to_head_diff=""
+local upstream_to_head="$(git --no-pager log --oneline $upstream..HEAD 2>/dev/null)"
+if [[ -n "$upstream_to_head" ]]; then
+    upstream_to_head_diff="$(echo $upstream_to_head | wc -l)"
+fi
 
-	if [[ $arr[2] =~ 'Your branch' ]]; then
-		if [[ $arr[2] =~ 'ahead' ]]; then
-			__CURRENT_GIT_BRANCH_STATUS_AMOUNT=$(echo $arr[2] | cut -d' ' -f 9)
-			__CURRENT_GIT_BRANCH_STATUS='ahead'
-		elif [[ $arr[2] =~ 'diverged' ]]; then
-			local amount1=$(echo $arr[3] | cut -d' ' -f 4)
-			local amount2=$(echo $arr[3] | cut -d' ' -f 6)
-			__CURRENT_GIT_BRANCH_STATUS_AMOUNT="${amount1},${amount2}"
-			__CURRENT_GIT_BRANCH_STATUS='diverged'
-		else
-			__CURRENT_GIT_BRANCH_STATUS_AMOUNT=$(echo $arr[2] | cut -d' ' -f 8)
-			__CURRENT_GIT_BRANCH_STATUS='behind'
-		fi
-	fi
+local head_to_upstream_diff=""
+local head_to_upstream="$(git --no-pager log --oneline HEAD..$upstream 2>/dev/null)"
+if [[ -n "$head_to_upstream" ]]; then
+    head_to_upstream_diff="$(echo $head_to_upstream | wc -l)"
+fi
 
-	if [[ ! $st =~ 'nothing to commit' ]]; then
-		__CURRENT_GIT_BRANCH_IS_DIRTY='1'
-	fi
+if [[ -n "$branch" ]]; then
+    __CURRENT_GIT_BRANCH="$branch"
+else
+    __CURRENT_GIT_BRANCH='[no-branch]'
+fi
 
-	# branch info
-	__CURRENT_GIT_BRANCH_REMOTE="$(git config branch.${__CURRENT_GIT_BRANCH}.remote)"
-	__CURRENT_GIT_BRANCH_MERGE="$(git config branch.${__CURRENT_GIT_BRANCH}.merge  | sed 's,refs/heads/,,')"
+__CURRENT_GIT_BRANCH_REMOTE="$remote"
+__CURRENT_GIT_BRANCH_MERGE="$merge"
+
+if [[ -n "$upstream_to_head_diff" && -n "$head_to_upstream_diff" ]]; then
+    __CURRENT_GIT_BRANCH_STATUS_AMOUNT="${upstream_to_head_diff},${head_to_upstream_diff}"
+    __CURRENT_GIT_BRANCH_STATUS='diverged'
+elif [[ -n "$upstream_to_head_diff" ]]; then
+    __CURRENT_GIT_BRANCH_STATUS_AMOUNT="${upstream_to_head_diff}"
+    __CURRENT_GIT_BRANCH_STATUS='ahead'
+elif [[ -n "$head_to_upstream_diff" ]]; then
+    __CURRENT_GIT_BRANCH_STATUS_AMOUNT="${head_to_upstream_diff}"
+    __CURRENT_GIT_BRANCH_STATUS='behind'
+else
+    __CURRENT_GIT_BRANCH_STATUS_AMOUNT=""
+    __CURRENT_GIT_BRANCH_STATUS='clean'
+fi
+
+# performance bottle-neck, calls git status internally
+local diff_files="$(git diff-files --exit-code)"
+if [[ -n "$diff_files" ]]; then
+    __CURRENT_GIT_BRANCH_IS_DIRTY='1'
 fi
