922b2bbbe03e3719de9c124d423c92b142824770
Kevin Funk <krf@electrostorm.net>
2012-01-04 20:29:59 +0100

    Set 'raster' as graphics system in Qt

--- .kde/env/profile.sh
+++ .kde/env/profile.sh
@@ -13,7 +13,10 @@ fi
 export GRE_HOME=/tmp
 
 # fix nvidia card painting issues
-VGA_INFO="$(lspci | grep "VGA")"
-#if [[ "$VGA_INFO" =~ "nVidia" ]]; then
-#    export QT_GRAPHICSSYSTEM=raster
-#fi
+VGA_INFO="$(lspci | grep 'VGA')"
+if [[ "$?" -ne "0" ]]; then
+    VGA_INFO="$(/sbin/lspci | grep 'VGA')"
+fi
+if [[ "$VGA_INFO" =~ "nVidia" ]]; then
+    export QT_GRAPHICSSYSTEM=raster
+fi
