9ab07375b006c112f53a83133124228c2d058614
Kevin Funk <kevin@kfunk.org>
2012-12-19 15:52:08 +0100

    Greatly simplify apt-purge-other-kernels

--- bin/apt-purge-other-kernels.py
+++ bin/apt-purge-other-kernels.py
@@ -2,28 +2,14 @@
 
 import os, sys
 
-def get_old_kernel_versions(runningKernel):
-    oldKernelVersions = []
-    for kernel in os.popen("ls /boot/vmlinuz*").readlines():
-        kernel = kernel.replace("/boot/vmlinuz-", "").replace("-generic", "").strip()
-        if kernel == runningKernel:
-           continue # do not add running kernel
-
-        if kernel:
-            oldKernelVersions.append(kernel)
-    return oldKernelVersions
-
-def get_packages_from_kernel_versions(kernels):
+def get_kernel_packages():
     packages = []
-    for kernel in kernels:
-        # remove suffixes like "-generic-pae" from 2.6.35-23-generic-pae
-        version = '-'.join(kernel.split('-')[:2])
 
-        # build packages list
-        cmd = "dpkg --get-selections *{0}*".format(version)
-        for package in os.popen(cmd).readlines():
-            package = package.split("\t")[0]
-            packages.append(package)
+    # build packages list, get all kernel packages
+    cmd = "dpkg --get-selections \"linux-*-*.*.*\""
+    for line in os.popen(cmd).readlines():
+        package = line.split("\t")[0]
+        packages.append(package)
 
     return packages
 
@@ -31,21 +17,20 @@ def get_running_kernel_version():
     return os.popen("uname -r").read().replace("-generic","").strip()
 
 def main():
-    runningKernel = get_running_kernel_version()
-    print("Current kernel: {0}".format(runningKernel))
+    running_kernel_version = get_running_kernel_version()
+    print("Current kernel: {0}".format(running_kernel_version))
 
-    oldKernelVersions = get_old_kernel_versions(runningKernel)
-    if len(oldKernelVersions) > 0:
-        packages = get_packages_from_kernel_versions(oldKernelVersions)
-        if len(packages) > 0:
-            cmd = ["sudo", "aptitude", "purge", "-P"]
-            cmd += packages
+    packages = get_kernel_packages()
 
-            print("Executing '{0}'".format(" ".join(cmd)))
-            os.execvp(cmd[0], cmd[0:])
-        else:
-            print("Old kernels found but no packages matched. Exit.")
-            sys.exit(1)
+    # filter out current kernel
+    packages = [pkg for pkg in packages if not running_kernel_version in pkg]
+
+    if len(packages) > 0:
+        cmd = ["sudo", "aptitude", "purge", "-P"]
+        cmd += packages
+
+        print("Executing '{0}'".format(" ".join(cmd)))
+        os.execvp(cmd[0], cmd[0:])
     else:
         print("No old kernels found. Exit.")
         sys.exit(0)
