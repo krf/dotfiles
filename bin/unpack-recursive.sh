119b1f3130209b3d033c57f36b508ebdb545c3df
Kevin Funk <kevin@kfunk.org>
2013-06-17 23:26:21 +0200

    Update unpack-recursive.sh script

--- bin/unpack-recursive.sh
+++ bin/unpack-recursive.sh
@@ -1,5 +1,29 @@
-#!/bin/sh
-for dir in ./*/
-do
-    unrar e -o- $dir/*.rar
+#!/bin/bash
+#
+# Unpack all files in a directory or for a list of files
+#
+# Usage: unpack-recursive.sh [FILE]...
+#   (uses current directory when no arguments are given)
+
+FILES="$@"
+
+if [ -z "$FILES" ]; then
+    FILES="*"
+fi
+
+for file in $FILES; do
+    echo "Checking file: $file"
+
+    if [[ "$file" == *.tar ]]; then
+        echo "Untaring file: $file"
+        tar xf $file &&
+            echo "Deleting file: $file" &&
+            rm $file
+    fi
+
+    if [ -d "$file" ]; then
+        unrar e -o- $dir/*.rar
+    fi
 done
+
+exit 0
