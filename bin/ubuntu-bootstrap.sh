f0b2e7223876caa3a6f1044c3eee7139a9a945d1
Kevin Funk <kfunk@kde.org>
2014-12-28 13:11:08 +0100

    Update

--- bin/ubuntu-bootstrap.sh
+++ bin/ubuntu-bootstrap.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-sudo apt-get install -y ack-grep cgdb git kdesdk-scripts vim zsh \
+sudo apt-get install -y ack-grep cgdb git kdesdk-scripts pastebinit vim zsh \
     chromium-browser quassel-client pidgin virtualbox yakuake zim \
     build-essential clang-3.5 \
-    linux-tools-common \
+    linux-tools-common mesa-utils \
     gperf `# for Qt5 webkit` \
     libjson-perl libxml-parser-perl `# for kdesrc-build` \
 
