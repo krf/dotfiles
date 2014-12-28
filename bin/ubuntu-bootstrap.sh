df9706c4bb7e279ccd301ae25e1acbe028a06393
Kevin Funk <kfunk@kde.org>
2014-12-28 11:19:49 +0100

    Update

--- bin/ubuntu-bootstrap.sh
+++ bin/ubuntu-bootstrap.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 sudo apt-get install -y ack-grep cgdb git kdesdk-scripts vim zsh \
-    chromium-browser quassel-client pidgin yakuake zim \
+    chromium-browser quassel-client pidgin virtualbox yakuake zim \
     build-essential clang-3.5 \
     linux-tools-common \
+    gperf `# for Qt5 webkit` \
     libjson-perl libxml-parser-perl `# for kdesrc-build` \
 
 sudo apt-get build-dep -y kdelibs5-dev qt5-default
