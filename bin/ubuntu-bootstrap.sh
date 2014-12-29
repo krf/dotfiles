c6986b51248cef6961ae01f0c5759407c7694e9e
Kevin Funk <kfunk@kde.org>
2014-12-29 11:52:10 +0100

    Update

--- bin/ubuntu-bootstrap.sh
+++ bin/ubuntu-bootstrap.sh
@@ -6,5 +6,6 @@ sudo apt-get install -y ack-grep cgdb git kdesdk-scripts pastebinit vim zsh \
     linux-tools-common mesa-utils \
     gperf `# for Qt5 webkit` \
     libjson-perl libxml-parser-perl `# for kdesrc-build` \
+    libboost-dev `# for kdevelop` \
 
 sudo apt-get build-dep -y kdelibs5-dev qt5-default
