600beb4ee7246c3062f1e8ebadb84d403f05e06b
Kevin Funk <kfunk@kde.org>
2015-01-06 15:54:44 +0100

    Update

--- bin/ubuntu-bootstrap.sh
+++ bin/ubuntu-bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt-get install -y ack-grep acpi bzr build-essential clang-3.5 cgdb git git-svn imagemagick kdesdk-scripts kdiff3 linux-tools-common mercurial mesa-utils nethogs ninja-build pastebinit powertop valgrind vim zsh `# CLI` \
+sudo apt-get install -y ack-grep acpi bzr build-essential clang-3.5 cgdb deborphan git git-svn imagemagick inxi kdesdk-scripts kdiff3 linux-tools-common mercurial mesa-utils nethogs ninja-build pastebinit powertop valgrind vim wajig zsh `# CLI` \
     chromium-browser gparted kde-baseapps-bin kleopatra quassel-client pidgin virtualbox yakuake zim `# GUI` \
     gperf `# for Qt5 webkit` \
     hunspell-de-de `# for libreoffice` \
