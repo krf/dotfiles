7f6bc19914be492d1a83aeeb593baa156487bda0
Kevin Funk <kfunk@kde.org>
2015-04-23 08:37:27 +0200

    Update

--- bin/ubuntu-bootstrap.sh
+++ bin/ubuntu-bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sudo apt-get install -y ack-grep acpi automake bzr build-essential clang-3.5 cgdb deborphan filelight gimp git git-svn imagemagick inxi kdesdk-scripts kdiff3 kolourpaint4 linux-tools-common mercurial mesa-utils nethogs ninja-build pandoc pastebinit powertop python-rbtools texlive-latex-base texlive-fonts-recommended valgrind vim wajig whois zsh `# CLI` \
-    chromium-browser filelight gparted kde-baseapps-bin kleopatra quassel-client pidgin virtualbox vlc yakuake zim `# GUI` \
+sudo apt-get install -y ack-grep acpi apt-file automake bzr build-essential ccache clang-3.5 cgdb deborphan filelight gimp git git-svn htop imagemagick inxi kdesdk-scripts kdiff3 kolourpaint4 linux-tools-common mercurial mesa-utils nethogs ninja-build pandoc pastebinit powertop python-rbtools sloccount texlive-latex-base texlive-fonts-recommended valgrind vim wajig whois zsh `# CLI` \
+    chromium-browser filelight gparted kde-baseapps-bin kleopatra quassel-client pidgin pidgin-plugin-pack virtualbox vlc yakuake zim `# GUI` \
     gperf `# for Qt5 webkit` \
     hunspell-de-de `# for libreoffice` \
     libgetopt-euclid-perl `# for git-blame-stats` \
