71cd474b11767479d84139a9d00d6724b0f95f55
Kevin Funk <kfunk@kde.org>
2023-03-31 15:24:46 +0200

    Update scripts

--- bin/ubuntu-bootstrap.sh
+++ bin/ubuntu-bootstrap.sh
@@ -7,12 +7,12 @@ sudo apt-get install -y ack silversearcher-ag acpi apt-file automake bzr build-e
     bison clang cmake lld flex g++ ninja-build qmlscene qt5-qmake `# building` \
     fonts-dejavu-extra `# for pandoc with mainfont DejavuSans` \
     gperf `# for Qt5 webkit` \
-    libxshmfence-dev/impish `# for Qt6 WebEngine` \
+    libxshmfence-dev `# for Qt6 WebEngine` \
     libdouble-conversion-dev libpcre2-dev `# for Qt5` \
     libreoffice-kde hunspell-de-de `# for libreoffice` \
     libgetopt-euclid-perl `# for git-blame-stats` \
     libjson-perl libxml-parser-perl `# for kdesrc-build` \
     libboost-thread-dev libcurlpp-dev libxerces-c-dev `# for libkolabxml` \
-    intltool libaspell-dev libboost-dev libdrm-dev libgif-dev libgl-dev libical-dev libegl-dev libfam-dev libgcrypt20-dev libphonenumber-dev libpolkit-gobject-1-dev libpolkit-agent-1-dev libpoppler-private-dev libxml2-dev libxslt-dev libsm-dev libssl-dev `# for Qt/KDE projects` \
+    intltool libaspell-dev libboost-dev libdrm-dev libgif-dev libgl-dev libical-dev libegl-dev libfam-dev libgcrypt20-dev libphonenumber-dev libpolkit-gobject-1-dev libpolkit-agent-1-dev libpoppler-private-dev "libxcb-*-dev" libxml2-dev libxslt-dev libsm-dev libssl-dev `# for Qt/KDE projects` \
 
-sudo apt-get build-dep -y kdevelop kmail kwin korganizer qt5-default
+sudo apt-get build-dep -y kmail kwin korganizer
